efc7e8d00ecbc345dc01614f467961d7fba83ca0
seh <02milkyway13@naver.com>
2023-02-09 10:37:47 +0900

    cicd test

---
+++ src/main/java/com/example/springboot7/Springboot7Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot7Application.class, args);
+	}
+
+}
